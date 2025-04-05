12ad81d83e5fb9b8ec8870021025a2242f6f428a
Opa Maj <saintkilla79@gmail.com>
2025-04-05 19:07:53 +0300

    Db

--- TouristPlaces.DataAccess/TouristPlacesDbContext.cs
+++ TouristPlaces.DataAccess/TouristPlacesDbContext.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TouristPlaces.DataAccess.Configurations;
+using TouristPlaces.DataAccess.Entities;
 
 namespace TouristPlaces.DataAccess
 {
-    internal class TouristPlacesDbContext
+    public class TouristPlacesDbContext : DbContext
     {
+        public TouristPlacesDbContext(DbContextOptions<TouristPlacesDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<PlaceEntity> Places { get; set; }
+
+        public DbSet<RegionEntity> Regions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new PlaceConfiguration());
+            modelBuilder.ApplyConfiguration(new RegionConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
